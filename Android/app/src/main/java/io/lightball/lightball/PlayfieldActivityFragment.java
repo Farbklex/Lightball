dd317cb1deeb899db91ca8efbfa163a7425a16f2
Alexander Hoffmann <aho@basecom.de>
2016-09-09 17:22:27 +0200

    Blank android project added

---
+++ Android/app/src/main/java/io/lightball/lightball/PlayfieldActivityFragment.java
@@ -0,0 +1,22 @@
+package io.lightball.lightball;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PlayfieldActivityFragment extends Fragment {
+
+    public PlayfieldActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_playfield, container, false);
+    }
+}
